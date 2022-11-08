3bce01aeb929ece4da0488ad02be7d005083a61d
Soybean <honghuangdc@gmail.com>
2022-11-08 16:11:54 +0800

    refactor(projects): remove @commitlint/config-conventional

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,22 @@
 /** @type {import('cz-git').UserConfig} */
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-leading-blank': [1, 'always'],
+    'body-max-line-length': [2, 'always', 100],
+    'footer-leading-blank': [1, 'always'],
+    'footer-max-line-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 100],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']
+    ]
+  },
   prompt: {
     alias: { fd: 'docs: fix typos' },
     messages: {
@@ -16,17 +32,18 @@ module.exports = {
       confirmCommit: '是否提交或修改commit ?'
     },
     types: [
-      { value: 'feat', name: 'feat:     新增功能 | A new feature' },
-      { value: 'fix', name: 'fix:      修复缺陷 | A bug fix' },
-      { value: 'docs', name: 'docs:     文档更新 | Documentation only changes' },
-      { value: 'style', name: 'style:    代码格式 | Changes that do not affect the meaning of the code' },
-      { value: 'refactor', name: 'refactor: 代码重构 | A code change that neither fixes a bug nor adds a feature' },
-      { value: 'perf', name: 'perf:     性能提升 | A code change that improves performance' },
-      { value: 'test', name: 'test:     测试相关 | Adding missing tests or correcting existing tests' },
-      { value: 'build', name: 'build:    构建相关 | Changes that affect the build system or external dependencies' },
-      { value: 'ci', name: 'ci:       持续集成 | Changes to our CI configuration files and scripts' },
-      { value: 'revert', name: 'revert:   回退代码 | Revert to a commit' },
-      { value: 'chore', name: 'chore:    其他修改 | Other changes that do not modify src or test files' }
+      { value: 'init', name: 'init:     初始化' },
+      { value: 'feat', name: 'feat:     新增功能' },
+      { value: 'fix', name: 'fix:      修复缺陷' },
+      { value: 'docs', name: 'docs:     文档更新' },
+      { value: 'style', name: 'style:    代码格式' },
+      { value: 'refactor', name: 'refactor: 代码重构' },
+      { value: 'perf', name: 'perf:     性能提升' },
+      { value: 'test', name: 'test:     测试相关' },
+      { value: 'build', name: 'build:    构建相关' },
+      { value: 'ci', name: 'ci:       持续集成' },
+      { value: 'revert', name: 'revert:   回退代码' },
+      { value: 'chore', name: 'chore:    其他修改' }
     ],
     useEmoji: false,
     emojiAlign: 'center',
