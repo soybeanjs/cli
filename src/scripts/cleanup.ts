6432b14d2ce18a125c7f8bbafebc40c7b337216c
Soybean <honghuangdc@gmail.com>
2023-01-05 20:10:24 +0800

    build(projects): fix cleanup script

--- src/scripts/cleanup.ts
+++ src/scripts/cleanup.ts
@@ -1,5 +1,5 @@
 import { $ } from 'zx';
 
 export async function cleanup() {
-  await $`pnpm rimraf node_modules ./**/node_modules dist ./**/dist pnpm-lock.yaml ./**/pnpm-lock.yaml`;
+  await $`pnpm rimraf node_modules dist package-lock.json yarn.lock pnpm-lock.yaml ./**/node_modules ./**/dist ./**/package-lock.json ./**/yarn.lock ./**/pnpm-lock.yaml`;
 }
