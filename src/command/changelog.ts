142ff1be50338497ed79db9bc767feaea16748de
Soybean <honghuangdc@gmail.com>
2023-06-07 02:35:01 +0800

    fix(projects): add githubToken for @soybeanjs/changelog

--- src/command/changelog.ts
+++ src/command/changelog.ts
@@ -1,9 +1,18 @@
+import { readFile } from 'fs/promises';
 import { generateChangelog, generateTotalChangelog } from '@soybeanjs/changelog';
 
-export function genChangelog(total = false) {
+export async function genChangelog(total = false) {
+  let githubToken = '';
+
+  try {
+    const pkgJson = await readFile(`${process.cwd()}/package.json`, 'utf-8');
+    const pkg = JSON.parse(pkgJson);
+    githubToken = pkg?.['github-token'] || '';
+  } catch {}
+
   if (total) {
-    generateTotalChangelog();
+    generateTotalChangelog({ github: { token: githubToken, repo: '' } });
   } else {
-    generateChangelog();
+    generateChangelog({ github: { token: githubToken, repo: '' } });
   }
 }
