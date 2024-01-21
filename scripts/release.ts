1dcaa70f12f24e2d5db0a03ef9b1ec7798ecb036
Soybean <soybeanjs@outlook.com>
2024-01-21 17:48:28 +0800

    feat(projects): add release script to update template version

---
+++ scripts/release.ts
@@ -0,0 +1,26 @@
+import process from 'node:process';
+import path from 'node:path';
+import { readFile, writeFile } from 'node:fs/promises';
+import fg from 'fast-glob';
+
+async function updateTemplateVersion() {
+  const cwd = process.cwd();
+
+  const globs = fg.sync(path.join(cwd, 'packages/create-soybean/template-*/package.json'));
+
+  const pkgJson = await readFile(path.join(cwd, 'package.json'), 'utf-8');
+
+  const { version: newVersion } = JSON.parse(pkgJson);
+
+  const soybeanJsCliVersionReg = /"@soybeanjs\/cli": "\d+\.\d+\.\d+(-(beta|alpha)\.\d+)?"/;
+
+  for await (const glob of globs) {
+    const json = await readFile(glob, 'utf-8');
+
+    const newPkgJson = json.replace(soybeanJsCliVersionReg, `"@soybeanjs/cli": "${newVersion}"`);
+
+    await writeFile(glob, newPkgJson);
+  }
+}
+
+updateTemplateVersion();
