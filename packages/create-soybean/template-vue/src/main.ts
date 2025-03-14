e31074ceb01c1a8124dff58df66bb48bb32e9faf
Soybean <soybeanjs@outlook.com>
2025-03-14 10:16:51 +0800

    chore(deps): update deps

--- packages/create-soybean/template-vue/src/main.ts
+++ packages/create-soybean/template-vue/src/main.ts
@@ -1,8 +1,6 @@
-import './assets/main.css';
-
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-
+import './assets/main.css';
 import App from './App.vue';
 import router from './router';
 
