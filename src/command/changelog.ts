34d8659e5f22bf680927e57e63416061d814c045
Soybean <soybeanjs@outlook.com>
2026-03-19 13:35:28 +0800

    fix(changelog): add check for existing CHANGELOG.md and add it to git if missing

--- src/command/changelog.ts
+++ src/command/changelog.ts
@@ -1,10 +1,20 @@
+import { join } from 'path';
+import { existsSync } from 'fs';
 import { generateChangelog, generateTotalChangelog } from '@soybeanjs/changelog';
 import type { ChangelogOption } from '@soybeanjs/changelog';
+import { execCommand } from '../shared';
 
 export async function genChangelog(options?: Partial<ChangelogOption>, total = false) {
+  const changelogPath = join(process.cwd(), 'CHANGELOG.md');
+  const hasChangelog = existsSync(changelogPath);
+
   if (total) {
     await generateTotalChangelog(options);
   } else {
     await generateChangelog(options);
+  }
+
+  if (!hasChangelog) {
+    await execCommand('git', [`add ${changelogPath}`]);
   }
 }
